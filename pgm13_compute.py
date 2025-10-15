b6be8df18280e4d5975173471a5baccfc0610f04
Thejalakshmi M M <thejalaxmi05@gmail.com>
2025-10-15 15:49:09 +0530

    Add files via upload

---
+++ pgm13_compute.py
@@ -0,0 +1,8 @@
+# Accept an integer n as input
+n=int(input("Enter an integer n: "))
+# Compute nn by multiplying n by 11
+nn=n*11
+# Compute nnn by multiplying n by 111
+nnn=n*111 
+s=n+nn+nnn 
+print(f"{n}+{nn}+{nnn}={s}")
